7119970b00026da1cd4bff23d95c11398362a89c
Mehtap Hisarciklilar <hisarciklilar@gmail.com>
2024-10-23 11:30:22 +0100

    feat: Add code to get the selected answer option by the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,23 +44,24 @@ const questions = [
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
-      button.addEventListener("click", function() {
-        if (button.getAttribute("id") === "submit") {
-            alert("You clicked submit");
-        } else if (button.getAttribute("id") === "next") {
-            // alert("You clicked next button!"); 
-            displayQuestion();
-        } else {
-            alert("you want to learn more");
-        }
-      }); // close of button event listener
+        button.addEventListener("click", function() {
+            if (button.getAttribute("id") === "submit") {
+                // alert("You clicked submit");
+                checkAnswer();
+            } else if (button.getAttribute("id") === "next") {
+                // alert("You clicked next button!"); 
+                displayQuestion();
+            } else {
+                alert("you want to learn more");
+            }
+        }); // close of button event listener
     } // close of loop across buttons
 }) // close of load page listener
 
-  // First question is displayed once the page loads
-  displayQuestion();
+// First question is displayed once the page loads
+displayQuestion();
 
-  function displayQuestion() {
+function displayQuestion() {
     let questionIndex = parseInt(document.getElementById("question-number").innerText);
     let questionNumber = questionIndex + 1;
     document.getElementById("question-number").textContent = `${questionNumber}`;
@@ -71,4 +72,19 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 }
 
-  
+// The idea of using "querySelectorAll" method and ".checked" property in the code below
+// is taken from an example provided in the following page:
+// https://www.javascripttutorial.net/javascript-dom/javascript-radio-button/
+function checkAnswer(){
+    let optionNumber=0
+    let questionOptions = document.querySelectorAll('input[name="option"]');
+    for (let questionOption of questionOptions) {
+        if (questionOption.checked) {
+            let selectedOptionNumber = optionNumber;
+            alert(`You selected option ${selectedOptionNumber}`)
+            break;
+        } else {
+            optionNumber++
+        }
+    }
+}
